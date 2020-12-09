21096858d1b7082611444d737665b656110693c5
tracywst0123 <69041079+tracywst0123@users.noreply.github.com>
2020-12-09 17:39:00 +0800

    - unused files deleted(test_table) - data updated till 2020/12/08

--- others.py
+++ others.py
@@ -1,7 +1,13 @@
-import numpy as np
 import pandas as pd
-import datetime
+from dash_utils import data_loader, quarter_target_table
 
 
 def convert_excel_time(excel_time):
     return pd.to_datetime('1899-12-30') + pd.to_timedelta(excel_time, 'D')
+
+
+if __name__ == '__main__':
+    data = data_loader()
+    table, d_str = quarter_target_table(data)
+
+    table.to_excel('/Users/tracy/Downloads/profit_table.xlsx')
