2b2c8e174a96cf4f6118693e00be9875b2d58324
tracywst0123 <69041079+tracywst0123@users.noreply.github.com>
2021-07-07 21:08:36 +0800

    Update users.py

--- gross_profit_dash_app/users.py
+++ gross_profit_dash_app/users.py
@@ -7,6 +7,4 @@ class User(UserMixin):
         self.password = None
 
 
-UserDict = {'admin': {'email': 'admin@email.com', 'password': '75168', 'authority': 'admin'},
-            'qiuyi': {'email': 'qiuyi@email.com', 'password': '12345', 'authority': ['075']},
-            'tracy': {'email': 'tracy@email.com', 'password': '12345', 'authority': ['010', '060']}}
+UserDict = {'tracy': {'email': 'tracy@email.com', 'password': '12345', 'authority': ['010']}}
